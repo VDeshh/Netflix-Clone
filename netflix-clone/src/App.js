b53e815cc0ec6cb7436ba6a6354b36de07bed41c
Vishal Desh <emailtovishy@gmail.com>
2020-07-18 01:04:58 -0700

    Banner Complete with CSS

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Vishal's Netflix Clone!</h1>
+    <div className="app">
+      {/* <h1>Vishal's Netflix Clone!</h1> */}
+      {/* Nav */}
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
+        isLargeRow={true}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchNetflixOriginals} />
