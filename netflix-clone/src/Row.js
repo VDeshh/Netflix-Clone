054ddac1ee7973be9b9a7582e30decb825767854
Vishal Desh <emailtovishy@gmail.com>
2020-07-17 17:43:52 -0700

    Use effect implemented with async function

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios'
 
-function row({ title }) {
+function row({ title, fetchUrl }) {
 
     const { movies, setMovies } = useState([]);
-    
+
+    useEffect(() => {
+        // if [], run once when the row loads, and don't run it again
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+            return request;
+        }
+        fetchData();
+    }, []);
 
     return (
         <div>
